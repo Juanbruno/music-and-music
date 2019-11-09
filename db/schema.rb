e28e76553f0c4b9ea628865e44b8f359383a5068
Juan bruno <jpruizk9@gmail.com>
2019-11-08 22:46:19 -0500

    creando medelos

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_11_09_034133) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.datetime "released_at"
+    t.integer "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_albums_on_artist_id"
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.integer "number"
+    t.string "preview_url"
+    t.integer "album_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["album_id"], name: "index_songs_on_album_id"
+  end
 
 end
